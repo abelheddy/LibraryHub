d33017b40351b6fa03ae77547978f586a225f60e
abel fuentes <abelmicz@hotmail.com>
2025-01-22 21:28:02 -0600

    creacion de los controladores y modificacion del codigo para crear el modulo de login

---
+++ LibraryHub/ViewModel/LoginViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LibraryHub.ViewModel
+{
+    public class LoginViewModel: ViewModelBase
+    {
+        //fields
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        public string Username
+        {
+            get => _username; 
+            set 
+            { 
+                _username = value; 
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public SecureString Password { get => _password; set  { _password = value; OnPropertyChanged(nameof(Password)); } }
+        public string ErrorMessage { get => _errorMessage; set { _errorMessage = value; OnPropertyChanged(nameof(ErrorMessage)); } }
+        public bool IsViewVisible { get => _isViewVisible; set  { _isViewVisible = value; OnPropertyChanged(nameof(IsViewVisible)); } }
+
+        //-> comandos
+        public ICommand LoginCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        //Constructor
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelCommand(p=>ExecuteRecoverPassCommand("",""));
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+ 
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if (string.IsNullOrWhiteSpace(Username) || Username.Length < 3 || Password == null || Password.Length < 3)
+            {
+                validData = false;
+            }
+            else
+            {
+                validData = true;
+            }
+            return validData;
+        } 
+
+        private void ExecuteRecoverPassCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
